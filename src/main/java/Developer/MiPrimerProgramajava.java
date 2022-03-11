3210cd7b11f6f5e6353b109c80c365b6e67fa468
YuleydiDelgado <yuleydiestefani@gmail.com>
2022-03-11 16:18:36 -0600

    mi primer programa java

---
+++ src/main/java/Developer/MiPrimerProgramajava.java
@@ -0,0 +1,44 @@
+
+package Developer;
+
+
+public class MiPrimerProgramajava {
+
+    public static void main(String[] args) {
+     package developer;
+
+public class MiPrimerProgramajava {
+
+    
+    public static void main(String[] args) {
+       
+        String nombreCompleto, direccion, estadoCivil;
+        int edad;
+        double gastosDiarios;
+        char genero;
+        boolean poseeVehiculo;
+                
+                nombreCompleto = "Camila Esperanza Dolores";
+                direccion= "calle de amargura";
+                estadoCivil= "soltera";
+                edad= 30;
+                gastosDiarios= 34.45;
+                genero='f';
+                poseeVehiculo= false;
+                
+              
+                System.out.println("Nombre Completo: "+ nombreCompleto);
+                 System.out.println("Direccion: "+ direccion);
+                  System.out.println("Estado Civil: "+estadoCivil);
+                   System.out.println("Edad: "+ edad);
+                    System.out.println("Gastos Diarios: "+ gastosDiarios);
+                     System.out.println("Genero: "+ genero);
+                      System.out.println("poseeVehiculo: "+ poseeVehiculo);
+                        
+        
+    }
+    
+}
+   
+    }
+
